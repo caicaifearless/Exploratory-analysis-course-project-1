328f7b100df80527e5e7958c3c4353a8774fd90a
caicaifearless <576392789@qq.com>
2019-07-15 14:45:38 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ alldata <- read.table("household_power_consumption.txt",header= TRUE,sep = ";",s
 data <- alldata[which(alldata$Date%in%c("1/2/2007","2/2/2007")),] 
 
 #plot png 1
-hist(as.numeric(data$Global_active_power),col = "red",main = "Global Active Power",ylab="Global_active_power",xlab=" ")
+hist(as.numeric(data$Global_active_power),col = "red",main = "Global Active Power",xlab="Global active power (kilowatts)")
 dev.copy(png,file="plot1.png")
 dev.off()
 
